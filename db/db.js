d4638203a1a42fc7abbeaf6068d1a49709869582
Bruno Perez <konobabuno@gmail.com>
2024-09-28 15:50:34 -0600

    db fix

--- db/db.js
+++ db/db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'mediterrum'
+    database: process.env.DB_NAME
   });  
 
 connection.connect((err) => {
