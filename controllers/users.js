8884d716c6956ba9d7c593b86974344ae073084d
edomedo <edramede@gmail.com>
2024-09-30 14:45:28 -0600

    modificar usuario vendedor

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ const obtenerUsuariosTodos = (req, res) => {
   });
 };
 
-// Obtener todos los usuarios
+// Obtener un usuario
 const obtenerUsuario = (req, res) => {
   const { id } = req.params;
 
@@ -29,6 +29,32 @@ const obtenerUsuario = (req, res) => {
     }
 
     const query = `CALL obtener_usuario(?)`;
+    
+    connection.query(query, [id], (err, results) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      res.status(201).json(results);
+    });
+  });
+};
+
+// Obtener la red de un usuario
+const obtenerUsuarioRed = (req, res) => {
+  const { id } = req.params;
+
+  const checkQuery = 'SELECT COUNT(*) as count FROM usuarios WHERE id = ?';
+
+  connection.query(checkQuery, [id], (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    if (results[0].count === 0) {
+      return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+    }
+
+    const query = `CALL obtener_usuario_red(?)`;
     
     connection.query(query, [id], (err, results) => {
       if (err) {
@@ -105,7 +131,7 @@ const modificarUsuarioDatos = (req, res) => {
   const { nuevoNombre, nuevoEmail, nuevoTelefono, nuevaLocacion } = req.body;
 
   if (isNaN(id)) {
-      return res.status(400).json({ error: 'ID inválido.' });
+      return res.status(400).json({ mensaje: 'ID inválido.' });
   }
 
   const query = `CALL modificar_usuario_datos(?, ?, ?, ?, ?)`;
@@ -131,7 +157,7 @@ const modificarUsuarioRol = (req, res) => {
   const { nuevoRol } = req.body;
 
   if (isNaN(id) || !['vendedor', 'promotor', 'distribuidor'].includes(nuevoRol)) {
-    return res.status(400).json({ error: 'ID inválido o rol no válido.' });
+    return res.status(400).json({ mensaje: 'ID inválido o rol no válido.' });
   }
 
   const checkCurrentRoleQuery = 'SELECT rol FROM usuarios WHERE id = ?';
@@ -163,12 +189,105 @@ const modificarUsuarioRol = (req, res) => {
   });
 };
 
+// Modificar la contraseña de un usuario
+const modificarUsuarioContrasena = (req, res) => {
+  const id = req.params.id;
+  const { nuevaContrasena } = req.body;
+
+  if (isNaN(id)) {
+    return res.status(400).json({ mensaje: 'ID inválido.' });
+  }
+
+  const checkUserQuery = `SELECT contrasena FROM usuarios WHERE id = ?`;
+  
+  connection.query(checkUserQuery, [id], (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ mensaje: 'Usuario no encontrado.' });
+    }
+
+    const currentPassword = results[0].contrasena;
+
+    if (currentPassword === nuevaContrasena) {
+      return res.status(400).json({ mensaje: 'La nueva contraseña no puede ser la misma que la actual.' });
+    }
+
+    const query = `CALL modificar_usuario_contrasena(?, ?)`;
+    const values = [id, nuevaContrasena];
+
+    connection.query(query, values, (err, results) => {
+      if (err) {
+        return res.status(500).json({ mensaje: err.sqlMessage });
+      }
+      res.status(200).json({ mensaje: `Contraseña de usuario con ID ${id} actualizada correctamente` });
+    });
+  });
+};
+
+// Modificar el vendedor de un usuario
+const modificarUsuarioVendedor = (req, res) => {
+  const usuario_id = req.params.id;
+  const { nuevoVendedor } = req.body;
+
+  if (isNaN(usuario_id) || (nuevoVendedor !== null && isNaN(nuevoVendedor))) {
+    return res.status(400).json({ mensaje: 'ID inválido.' });
+  }
+
+  if (nuevoVendedor === null) {
+    return res.status(400).json({ mensaje: 'El nuevo vendedor debe ser proporcionado.' });
+  }
+
+  const checkUsuarioQuery = `SELECT rol FROM usuarios WHERE id = ?`;
+  connection.query(checkUsuarioQuery, [usuario_id], (err, usuarioResults) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    if (usuarioResults.length === 0) {
+      return res.status(404).json({ mensaje: 'Usuario no encontrado.' });
+    }
+
+    const usuarioRol = usuarioResults[0].rol;
+    if (usuarioRol !== 'promotor') {
+      return res.status(400).json({ mensaje: 'El vendedor solo puede ser asignado a un promotor.' });
+    }
+
+    const checkVendedorQuery = `SELECT 1 FROM usuarios WHERE id = ? AND rol = 'vendedor'`;
+    connection.query(checkVendedorQuery, [nuevoVendedor], (err, vendedorResults) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      if (vendedorResults.length === 0) {
+        return res.status(404).json({ mensaje: 'Vendedor no encontrado.' });
+      }
+
+      const query = `CALL modificar_usuario_vendedor(?, ?)`;
+      const values = [usuario_id, nuevoVendedor];
+
+      connection.query(query, values, (err, results) => {
+        if (err) {
+          return res.status(400).json({ mensaje: err.sqlMessage });
+        }
+        res.status(200).json({ mensaje: `Vendedor del usuario con ID ${usuario_id} actualizado correctamente` });
+      });
+    });
+  });
+};
+
+
 
   module.exports = {
     insertarUsuario,
     obtenerUsuariosTodos,
     obtenerUsuario,
+    obtenerUsuarioRed,
     eliminarUsuario,
     modificarUsuarioDatos,
-    modificarUsuarioRol
+    modificarUsuarioRol,
+    modificarUsuarioContrasena,
+    modificarUsuarioVendedor
   };
--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,9 @@ router.get('/users', usersController.obtenerUsuariosTodos);
 // Ruta para obtener un usuario por ID
 router.get('/users/:id', usersController.obtenerUsuario);
 
+// Ruta para obtener la red de un usuario
+router.get('/users/:id/network', usersController.obtenerUsuarioRed);
+
 
 // Ruta para insertar un usuario
 router.post('/users', usersController.insertarUsuario);
@@ -22,5 +25,12 @@ router.put('/users/:id', usersController.modificarUsuarioDatos);
 
 // Ruta para modificar el rol de un usuario por ID (protegida)
 router.put('/users/:id/role', usersController.modificarUsuarioRol);
+
+// Ruta para modificar el rol de un usuario por ID (protegida)
+router.put('/users/:id/pwd', usersController.modificarUsuarioContrasena);
+
+// Ruta para modificar el vendedor de un usuario
+router.put('/users/:id/seller', usersController.modificarUsuarioVendedor);
+
 
 module.exports = router;
