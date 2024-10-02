9f7c3f02659f060a61ee2ddfbe04be61424dacb4
Bruno Perez <konobabuno@gmail.com>
2024-10-01 22:38:57 -0600

    fix cors

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ require('dotenv').config();
 
 // Middleware for parsing JSON
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*', // Permitir desde cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 // Routes
 app.use('/usuarios', userRoutes);
