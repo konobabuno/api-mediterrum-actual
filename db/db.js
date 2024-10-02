b91b85458e0a2bc17f430412276c82c747c11300
edomedo <edramede@gmail.com>
2024-10-02 08:37:04 -0600

    start test

--- db/db.js
+++ db/db.js
@@ -2,16 +2,27 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD_LOCAL,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    port: process.env.DB_PORT
 });
 
+
 // Exportamos directamente el pool sin promesas
-module.exports = pool;
+// module.exports = pool;
+
+module.exports = connection;
