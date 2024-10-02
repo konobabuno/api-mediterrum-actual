9f21ebca2c4ce316835b121ba49c21e5cbfe2c84
Bruno Perez <konobabuno@gmail.com>
2024-10-01 22:25:05 -0600

    fix conection

--- db/db.js
+++ db/db.js
@@ -2,27 +2,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-// const connection = mysql.createConnection({
-//   host: '127.0.0.1',
-//   user: 'root',
-//   password: process.env.DB_PASSWORD_LOCAL,
-//   database: 'mediterrum'
-// });
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error conectando a la base de datos: ' + err.stack);
-    return;
-  }
-  console.log('Conectado a la base de datos.');
-});
-
-module.exports = connection;
+// Exportamos directamente el pool sin promesas
+module.exports = pool;
