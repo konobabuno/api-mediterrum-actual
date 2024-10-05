197c35e271af339ea9a89b1fdd5ea842457f7f70
Bruno Perez <konobabuno@gmail.com>
2024-10-04 22:53:41 -0600

    distribuidores

--- controllers/dist-vend.js
+++ controllers/dist-vend.js
@@ -2,18 +2,20 @@ const connection = require('../db/db');
 
 // Obtener la lista de distribuidores
 const obtenerListaDistribuidores = (req, res) => {
-    const query = 'CALL obtener_lista_distribuidores()';
+    const query = 'SELECT id, nombre FROM usuarios WHERE rol = "distribuidor"';
 
     connection.query(query, (err, results) => {
         if (err) {
             return res.status(500).send(err);
         }
 
-        if (results[0].length === 0) {
+        // Si no hay distribuidores, retorna el mensaje adecuado
+        if (results.length === 0) {
             return res.status(404).send({ message: 'No distributors found' });
         }
 
-        res.status(200).json(results[0]); 
+        // Retorna todos los distribuidores en el arreglo 'results'
+        res.status(200).json(results); 
     });
 };
 
