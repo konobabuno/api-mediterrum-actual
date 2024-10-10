96c637e75cb3cee14f70bdd577f81661d401912f
edomedo <edramede@gmail.com>
2024-10-10 17:18:50 -0600

    delete restriction for user sales

--- controllers/users.js
+++ controllers/users.js
@@ -401,11 +401,37 @@ const eliminarUsuario = (req, res) => {
         return res.status(400).json({ mensaje: 'No se puede eliminar el usuario porque tiene ventas asociadas' });
       }
 
-      const deleteUsuarioQuery = 'CALL eliminar_usuario(?)';
+      // Obtener el carrito del usuario y actualizarlo a NULL
+      const carritoNullQuery = 'UPDATE carrito SET usuario = NULL WHERE usuario = ?';
 
-      connection.query(deleteUsuarioQuery, [id], (err) => {
+      connection.query(carritoNullQuery, [id], (err) => {
         if (err) return res.status(500).send(err);
-        return res.status(200).json({ mensaje: 'Usuario eliminado correctamente' });
+
+        // Eliminar los productos del carrito
+        const carritoProductoQuery = `
+          DELETE carrito_producto
+          FROM carrito_producto
+          JOIN carrito ON carrito_producto.carrito = carrito.id
+          WHERE carrito.usuario IS NULL`;
+
+        connection.query(carritoProductoQuery, (err) => {
+          if (err) return res.status(500).send(err);
+
+          // Eliminar historial del usuario
+          const historialQuery = 'DELETE FROM historial WHERE usuario = ?';
+
+          connection.query(historialQuery, [id], (err) => {
+            if (err) return res.status(500).send(err);
+
+            // Finalmente, llamar al procedimiento para eliminar el usuario
+            const deleteUsuarioQuery = 'CALL eliminar_usuario(?)';
+
+            connection.query(deleteUsuarioQuery, [id], (err) => {
+              if (err) return res.status(500).send(err);
+              return res.status(200).json({ mensaje: "Usuario eliminado correctamente" });
+            });
+          });
+        });
       });
     });
   });
